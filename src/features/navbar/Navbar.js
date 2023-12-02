c449ced9b8a09e67357524f13dad7f9cb7930ddb
puneet <puneet25j@gmail.com>
2023-12-02 10:26:48 +0530

    Update link prop

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -160,7 +160,7 @@ function NavBar({ children }) {
                     <Disclosure.Button
                       key={item.name}
                       as="a"
-                      href={item.href}
+                      href={item.link}
                       className={classNames(
                         item.current
                           ? 'bg-gray-900 text-white'
@@ -213,7 +213,7 @@ function NavBar({ children }) {
                       <Disclosure.Button
                         key={item.name}
                         as="a"
-                        href={item.href}
+                        href={item.link}
                         className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
                       >
                         {item.name}
